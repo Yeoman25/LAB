8cac8e3dcb65c6c4bd0b35e9b289fe8e5ce8bdad
Yeoman <ilinegser@mail.ru>
2025-10-13 21:14:55 +0300

    Умк пумк

--- Labs/Form1.cs
+++ Labs/Form1.cs
@@ -9,7 +9,7 @@ namespace Labs
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.BackColor = Color.Aquamarine;
         }
 
         private void button1_Click(object sender, EventArgs e)
