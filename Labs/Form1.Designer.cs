b7a3cab441e3e06e836a9cbf31c0b287fc2f77dd
Yeoman <ilinegser@mail.ru>
2025-10-13 21:31:30 +0300

    Пумк Умк

--- Labs/Form1.Designer.cs
+++ Labs/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             label1 = new Label();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -51,11 +55,51 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(140, 65);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 25);
+            button2.TabIndex = 2;
+            button2.Text = "Кнопка Вадима";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(268, 65);
+            button3.Name = "button3";
+            button3.Size = new Size(122, 24);
+            button3.TabIndex = 3;
+            button3.Text = "Кнопка Сани";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(140, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(268, 47);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 5;
+            label3.Text = "label3";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
@@ -69,5 +113,9 @@
 
         private Label label1;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private Label label3;
     }
 }
